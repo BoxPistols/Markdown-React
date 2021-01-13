72cb9a70169ccfd30dc6df08c40f3d8e63d7c3b5
ait <ait@ex.in>
2021-01-13 21:30:08 +0900

    create custom hook

---
+++ src/hooks/use_state_with_storage.ts
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export const useStateWithStorage = (
+    init: string,
+    key: string
+): [string, (s: string) => void] => {
+    const [value, setValue] = useState<string>(
+        localStorage.getItem(key) || init
+    )
+
+    const setStateWithStorage = (
+        nextValue: string
+    ): void => {
+        setValue(nextValue)
+        localStorage.setItem(key, nextValue)
+    }
+
+    return [value, setStateWithStorage]
+}
